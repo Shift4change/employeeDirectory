2c34306aecc00acf19e9008569b781170b64ff44
Paul Fodrovics <pefodrovics@gmail.com>
2020-04-02 20:41:41 -0400

    components created

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+return(
+    <>
+    <h1> Employee Directory </h1>
+    <p>click on  .... to filter by heading or use the search box to narrow your results.   </p>
+    </>
+)
+
+}
+
+export default Header;
---
+++ src/components/Search/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Search extends React.Component{
+    render() {
+        return(
+            <>
+            
+            <input type="text" id="searchBox" placeholder="Search" />
+
+
+
+            </>
+        );
+    }
+
+}
+
+export default Search;
+
+
