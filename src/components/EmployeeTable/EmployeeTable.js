770cdc1c055870b5657ea8ff610e6499943da187
Paul Fodrovics <pefodrovics@gmail.com>
2020-04-03 22:02:40 -0400

    published

--- src/components/EmployeeTable/EmployeeTable.js
+++ src/components/EmployeeTable/EmployeeTable.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./style.css";
 import Axios from "axios";
+import SearchForm from "../SearchForm/SearchForm"
 
 
 
 export default class EmployeeTable extends React.Component {
     state = {
-        people: []
+        people: [],
+        search: "",
+        display: []
+
     }
     componentDidMount() {
         Axios.get("https://randomuser.me/api/?results=10")
@@ -17,110 +21,107 @@ export default class EmployeeTable extends React.Component {
             })
 
     }
-   
 
-    
 
 
     handleInputChange = (event) => {
-        
-        var people= people.results
-    
-        if (event.target.name.first === "search") {
-            const searchTerm = event.target.value.toLowerCase();
-            
-            let newList = people.filter((people) => {
-               return people.name.first.toLowerCase().includes(searchTerm);
+        const searchTerm = event.target.value.toLowerCase();
+        const newList = this.state.people.filter(person => {
+            const theName = person.name.first;
+            return theName.toLowerCase().includes(searchTerm);
         });
         this.setState({
-            people:newList
+            display: newList,
+            search: searchTerm
         });
     }
-}
-   
-   
 
-    sortByName = () => {
-        let sortedPeople = this.state.people.sort
-            ((a, b) => {
-                if (b.name.first > a.name.first) {
-                    return -1;
-                }
-                if (a.name.first > b.name.first){
-                    return 1;
-                }
-                return 0;
-            });
-            if(this.state.sortOrder ==="DESC"){
-                sortedPeople.reverse();
-                this.setState({ sortOrder:"ASC" });
 
-            }else{
-                this.setState({sortOrder: "DESC"})
+
+
+sortByName = () => {
+    const sortedPeople = new Array(...this.state.people)
+
+    const sortUsers = sortedPeople.sort((a,b)=>{
+            if (b.name.first > a.name.first) {
+                return -1;
             }
-        this.setState({ people: sortedPeople });
-    }
+            if (a.name.first > b.name.first) {
+                return 1;
+            }
+            return 0;
+        });
+    // if (this.state.sortOrder === "DESC") {
+    //     sortedPeople.reverse();
+    //     this.setState({ sortOrder: "ASC" });
 
-     // sort by Email
-  sortByEmail = () => {
+    // } else {
+    //     this.setState({ sortOrder: "DESC" })
+    // }
+    this.setState({ display: sortUsers });
+}
+
+// sort by Email
+sortByEmail = () => {
     let sortedPeople = this.state.people.sort((a, b) => {
-      if (b.email > a.email) {
-        return -1;
-      }
-      if (a.email > b.email) {
-        return 1;
-      }
+        if (b.email > a.email) {
+            return -1;
+        }
+        if (a.email > b.email) {
+            return 1;
+        }
 
-      return 0;
+        return 0;
     });
 
     if (this.state.sortOrder === "DESC") {
         sortedPeople.reverse();
         this.setState({ sortOrder: "ASC" });
-      } else {
+    } else {
         this.setState({ sortOrder: "DESC" });
-      }
-      console.log("sort by gender");
-      this.setState({ results: sortedPeople });
-    };
-  
+    }
+    console.log("sort by gender");
+    this.setState({ results: sortedPeople });
+};
 
-    render() {
 
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Image</th>
-                        <th>Name<button onClick={this.sortByName}>Sort</button></th>
-                        <th>Phone</th>
-                        <th>Email</th>
-                        <th>DOB</th>
-                    </tr>
-                </thead>
-                <tbody >
+render() {
+
+    return (
+        <table>
+            <SearchForm handler={this.handleInputChange} val={this.state.search}></SearchForm>
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Name<button onClick={this.sortByName}>Sort</button></th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>DOB</th>
+                </tr>
+            </thead>
+            <tbody >
                 {this.state.people.map((person) => {
-                    return(
-                
-                    <tr key={person.id.value}>
-                        <td><img src={person.picture.thumbnail} alt="thumb" /></td>
-                        <td>{person.name.first}</td>
-                        {/* <td>{person.name.last}</td> */}
-                        <td>{person.phone}</td>
-                        <td>{person.email}</td>
-                        <td>{person.dob.date}</td>
-                    </tr>
-               
+                    return (
+
+                        <tr key={person.id.value}>
+                            <td><img src={person.picture.thumbnail} alt="thumb" /></td>
+                            <td>{person.name.first}</td>
+                            {/* <td>{person.name.last}</td> */}
+                            <td>{person.phone}</td>
+                            <td>{person.email}</td>
+                            <td>{person.dob.date}</td>
+                        </tr>
+
                     )
 
 
                 })}
-                 </tbody>
+            </tbody>
 
 
 
-            </table>
-        )
-    }
+        </table>
+    )
+}
 
 }
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,68 +1,20 @@
 import React, {Component} from "react";
-//  import EmployeeTable from "./EmployeeTable/EmployeeTable";
-// class Search extends React.Component{
-//     render() {
-//         return(
-//             <>
-            
-            
-//             <input
-//             onChange={props.handleInputChange}
-//             value={props.search}
-//             name="search"
-//             type="text"
-//             id="searchBox"
-//             placeholder="Search" 
-//             />
-
-
-
-//             </>
-//         );
-//     }
-
-// }
-// var people=result
-
-// class SearchForm extends React.Component {
-//     state= {
-//         people:[],
-//     }
 
    
 
 
 
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-function SearchForm(props){
-   
-   
-   
-   
+export default function SearchForm(props){
    
     return(
         <>
         
-        
         <input
-        onChange={props.handleInputChange}
-        value={props.search}
+        onChange={props.handler}
+        value={props.val}
         name="search"
         type="text"
+        className="searchInput"
         id="searchBox"
         placeholder="Search" 
         />
@@ -74,6 +26,6 @@ function SearchForm(props){
 
 }
 
-export default SearchForm;
+
 
 
